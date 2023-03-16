9fc95f3584b7672e77e0d208c7ef6e137aae80f1
Alumno <danigalcol@gmail.com>
2023-03-16 11:46:08 +0100

    1º commit incluye la estructura, los dos atributos y el constructor

---
+++ Mascota.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Mascota here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Mascota
+{
+    // instance variables - replace the example below with your own
+    private String nombre;
+    private String tipoDeMascota;
+
+    /**
+     * Constructor for objects of class Mascota
+     */
+    public Mascota()
+    {
+        
+    }
+
+    
+}
