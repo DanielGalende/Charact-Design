8ef439fe8b977dc8951f1e49b95facd8e57b7052
Amor <amore.acecalrgmail.com>
2023-03-21 12:11:12 +0100

    Agregamos la clase Personaje y sustres atributos

---
+++ Personaje.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Personaje here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Personaje
+{
+    private String nombre;
+    private String profesion;
+    private Mascota mascota;
+   
+}
